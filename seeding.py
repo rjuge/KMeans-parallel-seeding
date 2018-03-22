5fb5fb9a1ea144ff96f4ca8fa869e7479fb765f6
Rémi Juge <remi_juge@hotmail.fr>
2018-03-22 23:03:27 +0100

    Add plot and correct seeding

--- seeding.py
+++ seeding.py
@@ -2,23 +2,9 @@
 
 import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
-import os
-import multiprocessing as mp
-
-class Sampler(object):
-    def __init__(self, C, l, distance, random_state):
-        self.C = C
-        self.l = l
-        self.distance = distance
-        self.random_state = random_state
-    def __call__(self, x):
-        d = dist(x, self.C, metric_=self.distance)**2
-        s = self.random_state.choice(x.shape[0], p=min(1, self.l*d)) # TODO: access sum
-        if(s==1):
-            return x
 
 def dist(X, Y, metric_):
-    return pairwise_distances(X, Y, metric=metric_)
+    return pairwise_distances(X, Y, metric=metric_)**2
 
 def overseeding(X, t, l, distance, random_state, weights):
     # initial center sampled randomly
@@ -27,25 +13,17 @@ def overseeding(X, t, l, distance, random_state, weights):
     # compute probabilities
     di = dist(X, C, metric_=distance)
     # normalise
-    q = np.minimum(np.ones(di.shape), di)
+    q = np.minimum(np.ones(di.shape), di/np.sum(di))
     q = np.squeeze(q/np.sum(q))
-    # multiprocessing pool
-    pool = mp.Pool(mp.cpu_count())
-    X_list = [np.expand_dims(X[i,:], axis=0) for i in range(X.shape[0])]
     for i in range(t):
-        # run pool
-        #cand_id = random_state.choice(X.shape[0], size=np.int(l*X.shape[0]/100), p=q).astype(np.intp)
-        sampler = Sampler(C, l, distance, random_state)
-        cand_id = pool.map(sampler, X_list) # TODO: check output
-        pool.close()
-        pool.join()
+        cand_id = random_state.choice(X.shape[0], size=np.int(l*X.shape[0]/100), p=q).astype(np.intp)
         # extract selected points
         C_tmp = X[cand_id]
         C = np.vstack([C, C_tmp])
         # update proba
         di = np.min(dist(X, C, metric_=distance), axis=1)
         # normalise
-        q = np.minimum(np.ones(di.shape), di)
+        q = np.minimum(np.ones(di.shape), di/np.sum(di))
         q = np.squeeze(q/np.sum(q))
     return C
 
@@ -69,7 +47,7 @@ def seeding_parallel(X, t, l, K, distance, random_state, weights=None):
 
 def seeding_plusplus(X, wc, K, distance, random_state):
     # init first center
-    row = random_state.choice(X.shape[0], p=(wc/wc.sum())[:,0])
+    row = random_state.choice(X.shape[0], p=(wc/np.sum(wc))[:,0])
     C = np.expand_dims(X[row, :].T, axis=0)
     # compute proba
     di = np.multiply(dist(X, C, metric_=distance), wc)
--- test_script.py
+++ test_script.py
@@ -2,12 +2,14 @@
 """
 Created on Wed Mar 21 17:07:15 2018
 
-@author: romain
+@author: Romain G. et Rémi J.
 """
-'kmeans sklearn, dataset sklearn, seeding_parallel dans inti'
-'error avec intertie'
-'mettre la vitesse'
-'verbose = true pour linitialisation de klearn'
+"""
+
+if t >= k use kmeans++
+else kmeans||
+
+"""
 
 from time import time
 import numpy as np
@@ -18,12 +20,14 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
+
 from seeding import*
 
 np.random.seed(42)
 
 digits = load_digits()
 data = scale(digits.data)
+#data = digits.data
 
 n_samples, n_features = data.shape
 n_digits = len(np.unique(digits.target))
@@ -37,69 +41,37 @@ print("n_digits: %d, \t n_samples %d, \t n_features %d"
 def bench_k_means(estimator, name, data):
     t0 = time()
     estimator.fit(data)
-    print(name, (time() - t0), estimator.inertia_)
+    print(name)
+    print('Time : ', (time() - t0))
+    print('Inertia : ', estimator.inertia_)
+    print('')
 
 print('_____init: KMEANS++ SKLEARN______')
-bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=1),
+bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
               name="k-means++", data=data)
 
 print('_____init: RANDOM______')
-bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=1),
+bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),
               name="random", data=data)
 
-print('_____init: KMEANS++implemented______')
+print('_____init: KMEANS||______')
 t0 = time()
-seeding = seeding_parallel(data,5, 20, n_digits, 'euclidean',random_state=None, weights=None)
-print((time() - t0))
+seeding = seeding_parallel(data, 5, 30, n_digits, 'euclidean', np.random.RandomState(), weights=None)
+print('Init time : ', (time() - t0))
 
 bench_k_means(KMeans(init=seeding, n_clusters=n_digits, n_init=1),
-              name="random", data=data)
+              name="k-means||", data=data)
 
-## in this case the seeding of the centers is deterministic, hence we run the
-## kmeans algorithm only once with n_init=1
-#pca = PCA(n_components=n_digits).fit(data)
-#bench_k_means(KMeans(init=pca.components_, n_clusters=n_digits, n_init=1),
-#              name="PCA-based",
-#              data=data)
-#
-#
-## #############################################################################
-## Visualize the results on PCA-reduced data
-#
-#reduced_data = PCA(n_components=2).fit_transform(data)
-#kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
-#kmeans.fit(reduced_data)
-#
-## Step size of the mesh. Decrease to increase the quality of the VQ.
-#h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
-#
-## Plot the decision boundary. For that, we will assign a color to each
-#x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-#y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-#xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-#
-## Obtain labels for each point in mesh. Use last trained model.
-#Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
-#
-## Put the result into a color plot
-#Z = Z.reshape(xx.shape)
-#plt.figure(1)
-#plt.clf()
-#plt.imshow(Z, interpolation='nearest',
-#           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-#           cmap=plt.cm.Paired,
-#           aspect='auto', origin='lower')
-#
-#plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
-## Plot the centroids as a white X
-#centroids = kmeans.cluster_centers_
-#plt.scatter(centroids[:, 0], centroids[:, 1],
-#            marker='x', s=169, linewidths=3,
-#            color='w', zorder=10)
-#plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
-#          'Centroids are marked with white cross')
-#plt.xlim(x_min, x_max)
-#plt.ylim(y_min, y_max)
-#plt.xticks(())
-#plt.yticks(())
-#plt.show()
+reduced_data = PCA(n_components=2).fit_transform(data)
+seeding = seeding_parallel(reduced_data, 5, 30, n_digits, 'euclidean', np.random.RandomState(), weights=None)
+for i in ['k-means++', 'random', seeding]:
+    kmeans = KMeans(init=seeding, n_clusters=n_digits, n_init=1)
+    Y = kmeans.fit_predict(reduced_data)
+    c = ['b','g','r','m','y','k','c','0.25', '0.5', '0.75']
+    plt.figure()
+    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], color=[c[j] for j in Y])
+    C = PCA(n_components=2).fit_transform(kmeans.cluster_centers_)
+    plt.scatter(C[:, 0], C[:, 1], s=140,  marker="*")
+    plt.title(i)
+    plt.show()
+
